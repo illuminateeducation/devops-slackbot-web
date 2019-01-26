5b50d0eabedd1d14d8a56f7cba6f06a8effa0169
nick b <nbaer@illuminateed.net>
2019-01-25 21:22:59 -0700

    Optimize order of logic; add user to output of response message

--- www/jiraticket/ticket-linker.php
+++ www/jiraticket/ticket-linker.php
@@ -8,10 +8,28 @@
 include_once("../../includes/constants.php");		// Need constants: JIRA_TICKET_LINKER_SECRET and ATLASSIAN_PROJECT
 include_once("../../includes/slack_verify.php");	// Need function: is_slack_signed(...)
 
-// Configure variables
 $ticket_id = filter_input(INPUT_POST, "text");
+
+// Make sure $ticket_id is reasonable
+if(strlen($ticket_id) > 25)
+{
+	exit("Ticket ID is too long. Must be less than 25 characters");
+}
+
+// Make sure $ticket_id is in a valid format
+if(!preg_match("/^[a-zA-Z0-9]*-[0-9]*$/", $ticket_id))
+{
+	exit("Invalid ticket ID.");
+}
+
 $headers = apache_request_headers();
 $timestamp = $headers["X-Slack-Request-Timestamp"];
+
+// Make sure it's not stale/replay attempt (60 * 3 = 3min)
+if(abs(time() - $timestamp) > (60 * 3))
+{
+	exit("Request is stale, ignoring.");
+}
 
 $slack_signed = is_slack_signed(
 	$timestamp,
@@ -26,28 +44,14 @@ if(!$slack_signed)
 	exit("Invalid request.");
 }
 
-// Make sure it's not stale/replay attempt (60 * 3 = 3min)
-if(abs(time() - $timestamp) > (60 * 3))
-{
-	exit("Request is stale, ignoring.");
-}
-
-// Make sure $ticket_id is reasonable
-if(strlen($ticket_id) > 25)
-{
-	exit("Ticket ID is too long. Must be less than 25 characters");
-}
-
-// Make sure $ticket_id is in a valid format
-if(!preg_match("/^[a-zA-Z0-9]*-[0-9]*$/", $ticket_id))
-{
-	exit("Invalid ticket ID.");
-}
+$linker_username = filter_input(INPUT_POST, "user_name");
+$response_url = "https://".ATLASSIAN_PROJECT.".atlassian.net/browse/{$ticket_id}";
+$response_msg = "{$linker_username} linked a ticket: {$response_url}";
 
 // The actual response body
 $resp_arr = array(
 	"response_type" => "in_channel",
-	"text" => "https://".ATLASSIAN_PROJECT.".atlassian.net/browse/$ticket_id"
+	"text" => $response_msg
 );
 
 // Make the curl request

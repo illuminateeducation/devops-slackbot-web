e564b9c393b19726c92b22beba937c4992262d37
nick b <nbaer@illuminateed.net>
2019-01-25 19:26:26 -0700

    Fixed a comment

--- includes/constants.php
+++ includes/constants.php
@@ -3,7 +3,7 @@ define("ATLASSIAN_PROJECT", "<ATLASSIAN-PROJECT-SUBDOMAIN-HERE>");
 define("JIRA_TICKET_LINKER_SECRET", "<SLACK-APP-SIGNATURE-HERE>");
 
 // Other apps' secrets can be defined here:
-//define("");
-//define("");
-//define("");
+//define("", "");
+//define("", "");
+//define("", "");
 

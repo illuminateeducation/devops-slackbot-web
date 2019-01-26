0f2fd7119cac81f0d2bf95c5f8e4b99eb9e24806
nick b <nbaer@illuminateed.net>
2019-01-25 21:49:12 -0700

    Removed some ugly concatenation

--- includes/slack_verify.php
+++ includes/slack_verify.php
@@ -1,7 +1,7 @@
 <?php
 function is_slack_signed($timestamp, $slack_sig, $secret, $raw_body)
 {
-	$sig_basestring = "v0:".$timestamp.":".$raw_body;
+	$sig_basestring = "v0:{$timestamp}:{$raw_body}";
 	$signature = "v0=".hash_hmac("sha256", $sig_basestring, $secret);
 
 	return ($signature === $slack_sig)? true : false;
